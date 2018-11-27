44e38d869b10dcdc8fd54db7da7414a390198ee4
mir1995 <mir00021@hotmail.com>
2018-11-27 17:51:47 +0000

    fgdg

---
+++ criminal-activity/bipartite.py
@@ -0,0 +1,7 @@
+import networkx as nx 
+
+
+# path to cleaned file
+EVENTS_ATTENDANCE_CLEANED = "criminal-activity/datasets/cleaned/mafia_bipartite.csv"
+
+
---
+++ criminal-activity/cleaning.py
@@ -0,0 +1,8 @@
+"""This script cleans the csv file 
+   containing the network data. The main issue
+   is the header row which the name of the restaurant
+   comma separated by the date. 
+   Just convert the header to integers identifiers preseving
+   the chronological order of the events.
+"""
+import csv
