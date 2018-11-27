4167b773a21c345ae7812a2a2a5bde52faad68de
mir1995 <mir00021@hotmail.com>
2018-11-27 23:43:58 +0000

    cleaning the data

--- criminal-activity/cleaning.py
+++ criminal-activity/cleaning.py
@@ -8,6 +8,45 @@
 import csv
 
 
-EVENTS_ATTENDANCE_ORIGINAL = "criminal-activity/datasets/original/NDRANGHETAMAFIA_2M.csv"
+EVENTS_ATTENDANCE_ORIGINAL = "NetworkAnalysis/criminal-activity/datasets/original/NDRANGHETAMAFIA_2M.csv"
 # save cleaned file to a new file
-EVENTS_ATTENDANCE_CLEANED = "criminal-activity/datasets/cleaned/mafia_bipartite.csv"
+EVENTS_ATTENDANCE_CLEANED = "NetworkAnalysis/criminal-activity/datasets/cleaned/mafia_bipartite.csv"
+
+
+with open(EVENTS_ATTENDANCE_ORIGINAL, 'r') as f, open(EVENTS_ATTENDANCE_CLEANED, 'w') as w:
+   # reader - eyes
+   reader = csv.reader(f)
+   # skip header row
+   header = next(reader)
+   # set new header - 
+   new_header = ["surname", "forename", "fullname", "event"] 
+   # write new header, join takes a list and joins the items in this case separating them with a comma
+   w.write("{}\n".format(','.join(new_header)))
+   # go to second line
+   row = next(reader)
+   # while roe is non-empty
+   while row:
+      # get the full name of the person
+      fullname = row[0]
+      if fullname.split(' ')[0] == "DE":
+         surname = fullname.split(' ')[1].title()
+      else:
+         surname = fullname.split(' ')[0].title()
+      # split the name into surname and forename - ignore middle name
+      forename = fullname.split(' ')[-1]
+      # now count the numb of events attended
+      booleans = [int(boolean) for boolean in row[1:]] 
+      # which events did he participate to - the 1's
+      events = [event for event,boolean in enumerate(booleans) if boolean == 1]
+      for i in range(len(events)):
+         # set a new row
+         new_row = [forename, surname, fullname, str(events[i])]
+         # write new row to the file
+         w.write("{}\n".format(','.join(new_row)))
+      # move onto new row
+      try:
+         row = next(reader)
+      # if line is empty python raises an error
+      except StopIteration:
+         # in this case set the row to None - this way "while row" will give False
+         row = None
